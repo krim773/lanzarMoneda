dbb0b89ab24680296ba1723be6f19653258c2dff
krim773 <104527387+krim773@users.noreply.github.com>
2023-05-23 04:48:25 +0000

    Update LanzarMoneda.jsx

--- LanzarMoneda.jsx
+++ LanzarMoneda.jsx
@@ -4,8 +4,8 @@ function tossCoin() {
 
 function fiveHeads() {
   return new Promise((resolve, reject) => {
-    let headsCount = 0;
-    let attempts = 0;
+    let headsCount = 0; // Contador de "heads"
+    let attempts = 0; // Contador de intentos
 
     const flipCoin = () => {
       attempts++;
@@ -14,25 +14,27 @@ function fiveHeads() {
       if (result === "heads") {
         headsCount++;
       } else {
-        headsCount = 0;
+        headsCount = 0; // Reiniciar el contador si no se obtiene "heads"
       }
 
       if (headsCount === 5) {
-        resolve(`It took ${attempts} tries to flip five "heads"`);
+        resolve(`It took ${attempts} tries to flip five "heads"`); // Resolución exitosa de la promesa
       } else {
-        if (attempts >= 100) { // Limitar la cantidad de intentos para evitar bucles infinitos
-          reject("Unable to flip five \"heads\" within 100 attempts");
+        if (attempts >= 100) {
+          // Limitar la cantidad de intentos para evitar bucles infinitos
+          reject("Unable to flip five \"heads\" within 100 attempts"); // Rechazo de la promesa con un mensaje de error
         } else {
-          flipCoin();
+          flipCoin(); // Llamar recursivamente a flipCoin para seguir lanzando la moneda
         }
       }
     };
 
-    flipCoin();
+    flipCoin(); // Llamada inicial a flipCoin para iniciar el proceso de lanzamiento de la moneda
   });
 }
 
 fiveHeads()
-  .then((res) => console.log(res))
-  .catch((err) => console.log(err));
-console.log("When does this run now?");
+  .then((res) => console.log(res)) // Manejo del caso de éxito
+  .catch((err) => console.log(err)); // Manejo del caso de error
+console.log("When does this run now?"); // Se muestra por consola inmediatamente después de la llamada a fiveHeads
+
